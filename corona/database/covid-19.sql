b4a4d31b66cd4f29b5da060eb8342802bbebde07
newbietot <nasa.sly14@gmail.com>
2021-01-13 15:20:31 +0700

    update sql

--- corona/database/covid-19.sql
+++ corona/database/covid-19.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 12, 2021 at 04:18 PM
--- Server version: 5.6.38
--- PHP Version: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Jan 13, 2021 at 09:18 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -129,8 +128,11 @@ INSERT INTO `patients` (`id`, `name`, `street`, `municipality`, `dateOfBirth`, `
 (5, 'charlene april alves', 'quezon santa ilocos sur', '', '', '23', 'Female', '9063430174', '', '', '', '', '', '', '', '', '1606477399', 'negative', 'negative', '0639125096a1773b4b755df7848cc56f', 'kghjsdghasdghjfdfahj', '1517', 'Inpatient'),
 (7, 'julius ceasar cuaresma', 'vigan', '', '', '21', 'Male', '09361226566', '', '', '', '', '', '', '', '', '1606795126', 'negative', 'travel history', 'd7fcc162c9e41624ae66225431027e50', 'kghjsdghasdghjfdfahj', '1577', 'Inpatient'),
 (9, 'ghie kurt pazz', 'vigan city', '', '', '23', 'Male', '09556378057', '', '', '', '', '', '', '', '', '1607875286', 'negative', 'apor', '36f3b826b142a57e65e2a344ae88d8a1', 'kghjsdghasdghjfdfahj', '7977', 'Inpatient'),
-(10, 'marlon villena', 'mabilbila sur santa, ilocos sur', '', '23 - 11 - 1998', '23', 'Male', '09556378057', '', '', '', '', '', '', '', '', '1609988346', 'none\r<br />', 'none', '23d156526742db2e9807277fe8282604', 'cf7bc17bcf0caa3dbb2d4618e7338b96', '4103', 'Inpatient'),
-(11, 'lheander alves', 'mabilbila sur santa, ilocos sur', '', '11 - 02 - 2007', '14', 'Male', '09104724213', '2020-12-28', '2020-12-31', 'mabilbila sur santa, ilocos sur', 'pinili ilocos norte', 'boyet', 'motor', 'vc-1234', 'Yes', '1610076936', 'n/a', 'n/a', '823b08e1fee6fb6e96b6f2319a8bb1b5', 'cf7bc17bcf0caa3dbb2d4618e7338b96', '0', 'Inpatient');
+(11, 'lheander alves', 'mabilbila sur santa, ilocos sur', '', '11 - 02 - 2007', '14', 'Male', '09104724213', '2020-12-28', '2020-12-31', 'mabilbila sur santa, ilocos sur', 'pinili ilocos norte', 'boyet', 'motor', 'vc-1234', 'Yes', '1610076936', 'n/a', 'n/a', '823b08e1fee6fb6e96b6f2319a8bb1b5', 'cf7bc17bcf0caa3dbb2d4618e7338b96', '0', 'Inpatient'),
+(12, 'Sample', 'Street chu', 'Manila', '01 - 01 - 2000', '21', 'Male', '09123456789', '2021-01-23', '2021-01-11', 'n/a', 'n/a', 'Tester', 'Motor', 'AE1245', 'Yes', '1610508151', 'Negative', 'Noasdfthing', '218a6556bdc6baef7d9fd065bc74c84b', 'asdfghjkl', '1851', 'Inpatient'),
+(13, 'Sample Test', 'Street test', 'Manila', '21 - 02 - 2000', '21', 'Male', '09123456789', '2021-01-22', '2021-01-28', 'n/a', 'n/a', 'Tester', 'Car', 'BE2341', 'Yes', '1610508708', 'Positive', 'nothing', '265b8efd685ef791482f839783a2d000', 'asdfghjkl', '8295', 'Inpatient'),
+(14, 'baby', 'auto ', 'fill', '13 - 01 - 2011', '10', 'Male', '09123456789', '2021-01-08', '2021-01-28', 'n/a', 'n/a', 'Tester', 'Motor', 'BE2341', 'No', '1610514097', 'Positive', 'nothing', 'd14c902443253ab8a4dff202da0d065c', 'asdfghjkl', '7757', 'Inpatient'),
+(15, 'Ben Jamin', 'Auto', 'Fill', '2002-01-31', '18', 'Other', '09123456789', '2021-01-09', '2021-01-28', 'n/a', 'n/a', 'Tester', 'Motor', 'AE1245', 'No', '1610523982', 'Positive', 'Sample', '560400e7782291e45b825b342bbf2ec1', 'a43d2e0bee0a5dd5e8cc16f7e3e16423', '6894', 'Inpatient');
 
 -- --------------------------------------------------------
 
@@ -145,7 +147,7 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
-  `type` text NOT NULL,
+  `type` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `profile` varchar(255) DEFAULT NULL,
   `gender` varchar(255) NOT NULL,
@@ -159,7 +161,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `firstName`, `secondName`, `email`, `password`, `token`, `type`, `phone`, `profile`, `gender`, `role`, `license`) VALUES
 (6, 'Charles Novy', 'Alves', 'cnalves@gmail.com', 'alves', 'asdfghjkl', 'admin', '09556378057', NULL, 'male', 'Admin', ''),
-(19, 'Donnell John', 'Bueno', 'dj@gmail.com', 'bueno', '8d1b177eb8c2b3757d5d1fe8ca0f3d37', 'doctor', '09556378057', NULL, 'Male', 'Doctor', '1123');
+(53, 'John', 'Doe', 'john@gmail.com', 'ilocossur', 'a43d2e0bee0a5dd5e8cc16f7e3e16423', 'doctor', '09123456789', NULL, 'Male', 'Surgeon', '2415124124');
 
 --
 -- Indexes for dumped tables
@@ -221,13 +223,13 @@ ALTER TABLE `outbreaks`
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
